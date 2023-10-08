9684b7586cb2fad4f505d71c680cacb0ba79b131
Harsh <harshdivate45@gmail.com>
2023-10-09 02:39:00 +0530

    LocalStorage

--- [08]Shopping_List/script.js
+++ [08]Shopping_List/script.js
@@ -2,8 +2,9 @@ const itemForm=document.getElementById('item-form');
 const itemInput=document.getElementById('item-input');
 const itemList=document.getElementById('item-list');
 const clearAll=document.getElementById('clear');
-const itemfilter=document.getElementById('filter')
-
+const itemfilter=document.getElementById('filter');
+const butn=itemForm.querySelector('button');
+let isEditMode=false;
 
 
 
@@ -15,12 +16,36 @@ function addItem(e){
         return
     }
     const newItem=itemInput.value;
-    console.log(newItem);
-    const newListItem=createNewList(newItem);
-    itemList.appendChild(newListItem);
+   
+
+    addItemToDom(newItem);
+    addItemToStorage(newItem);
     checkUI();
     itemInput.value='';
     
+}
+function getItemsfromStorage(){
+    let itemFromLocalStorage;
+    
+    if(localStorage.getItem('items')===null){
+        //the array is empty
+        itemFromLocalStorage=[];
+    }else{
+        itemFromLocalStorage=JSON.parse(localStorage.getItem('items'));
+    }
+    return itemFromLocalStorage;
+}
+function addItemToStorage(item){
+    const itemFromLocalStorage=getItemsfromStorage();
+    itemFromLocalStorage.push(item);
+    localStorage.setItem('items',JSON.stringify(itemFromLocalStorage));
+    
+}
+
+function addItemToDom(newItem){
+    const newListItem=createNewList(newItem);
+    itemList.appendChild(newListItem);
+
 }
 
 function createNewList(itemName){
@@ -45,20 +70,51 @@ function createButton(){
 }
 
 
-function removeItem(e){
-    
-    if(e.target.tagName==='I'){
-        // const itemName=e.target.parentElement.parentElement.firstChild;
-        if(confirm(`Are you sure?`)){
-            e.target.parentElement.parentElement.remove();
-            checkUI();
-        }
-        else{
-            return;
-        }
-        
+function onClickItem(e){
+    if(e.target.parentElement.classList.contains('remove-item')){
+        removeItem(e.target.parentElement.parentElement);
     }
-    // console.log(e.target.tagName)
+    else{
+       setItemToEdit(e.target);
+    }
+}
+
+function setItemToEdit(item){
+    isEditMode=true;
+    item.style.color='#ccc';
+    const text=item.textContent;
+    butn.innerHTML='<i class="fa-solid fa-pen"></i>Update Item';
+    butn.style.backgroundColor='#228B22';
+    console.log(text);
+    itemInput.value=text;
+}
+
+function removeItem(item){
+    
+    if(confirm('Are you sure?')){
+        item.remove();
+    }
+    removeItemFromLocalStorage(item.textContent);
+   
+    
+}
+
+function removeItemFromLocalStorage(itemToRemove){
+    let itemsfromlocalStorage=getItemsfromStorage();
+
+    const newLocalStorage=removeItemFromStorageWithName(itemsfromlocalStorage,itemToRemove);
+    localStorage.setItem('items',JSON.stringify(newLocalStorage));
+    // localStorage.setItem('items',JSON.stringify(itemFromLocalStorage));
+}
+
+function removeItemFromStorageWithName(localStorage,itemName){
+    let newLocalStorage=[];
+    for(let i=0;i<localStorage.length;i++){
+        if(localStorage[i]!==itemName){
+            newLocalStorage.push(localStorage[i]);
+        }
+    }
+    return newLocalStorage;
 }
 
 
@@ -67,12 +123,29 @@ function removeAllItem(){
     while(itemList.firstChild){
         itemList.removeChild(itemList.firstChild);
     }
+
+    //remove from localstorage
+    localStorage.removeItem('items');
     checkUI();
+}
+
+function filterItem(e){
+    const text=e.target.value.toLowerCase();
+    const items=itemList.querySelectorAll('li');
+
+    items.forEach(item => {
+        const itemName=item.firstChild.textContent.toLowerCase();
+        if(itemName.includes(text)){
+            item.style.display='block';
+        }else{
+            item.style.display='none';
+        }
+    });
 }
 
 function checkUI(){
     const items=itemList.querySelectorAll('li');
-    console.log(items);
+    // console.log(items);
     if(items.length===0){
         clearAll.style.display='none';
         itemfilter.style.display='none';
@@ -82,12 +155,20 @@ function checkUI(){
     }
 }
 
+function displayItemsFromStorage(){
+    const itemFromLocalStorage=getItemsfromStorage();
+    itemFromLocalStorage.forEach(item =>{
+        addItemToDom(item);
+    })
+    checkUI();
+}   
+
 //Event listeneer
 itemForm.addEventListener('submit',addItem);
-itemList.addEventListener('click',removeItem);
+itemList.addEventListener('click',onClickItem);
 clearAll.addEventListener('click',removeAllItem);
-
-checkUI();
+itemfilter.addEventListener('input',filterItem);
+document.addEventListener('DOMContentLoaded',displayItemsFromStorage)
 
 
 
