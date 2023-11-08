1d5a8221a3ab95fa1e253baa9837f78c88cb7600
Harsh <harshdivate45@gmail.com>
2023-11-08 22:59:58 +0530

    Animated Clock Skip

--- [12] Web Browser APIs/CanvasAPI/script.js
+++ [12] Web Browser APIs/CanvasAPI/script.js
@@ -1,1 +1,23 @@
-const canvas = document.querySelector('#mycanvas');
+const image = document.querySelector('img');
+let start;
+let done = false;
+
+function step(timestamp){
+    if(start===undefined){
+        start=timestamp;
+    }
+    const elapsed = timestamp -start;
+    if(elapsed > 5000){
+        done = true ;
+    }
+    if(done ){
+        return;
+    }
+
+    image.style.transform = `translateX(${elapsed/20}px) rotate(${elapsed/20}deg)`;
+
+    requestAnimationFrame(step)
+
+}
+
+requestAnimationFrame(step);
